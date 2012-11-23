2d515c40c28adc1aa22025bb7fd1a685bfb43d17
Kirill Gusev <mr.gusev.k@gmail.com>
2012-11-23 15:27:34 +0400

    create projects

---
+++ GDCopter/GDCopter/GDCopter.cpp
@@ -0,0 +1,17 @@
+/*
+ * GDCopter.cpp
+ *
+ * Created: 23.11.2012 14:46:17
+ *  Author: Kirill
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
